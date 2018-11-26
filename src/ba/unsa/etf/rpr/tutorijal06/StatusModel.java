038f9335278e354272961fa3fce037cee9654648
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-26 16:29:03 +0100

    Napravljena klasa StatusModel

---
+++ src/ba/unsa/etf/rpr/tutorijal06/StatusModel.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr.tutorijal06;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class StatusModel {
+    private ObservableList<Status> statusi= FXCollections.observableArrayList();
+    private ObjectProperty<Status> trenutniStatus = new SimpleObjectProperty<>();
+
+    public Status getTrenutniStatus() {
+        return trenutniStatus.get();
+    }
+
+    public ObjectProperty<Status> trenutniStatusProperty() {
+        return trenutniStatus;
+    }
+
+    public void setTrenutniStatus(Status trenutniStatus) {
+        this.trenutniStatus.set(trenutniStatus);
+    }
+
+    public ObservableList<Status> getStatusi() {
+        return statusi;
+    }
+
+    void napuniStatuse(){
+        statusi.add(new Status("Redovan"));
+        statusi.add(new Status("Redovan samofinansirajući"));
+    }
+}
