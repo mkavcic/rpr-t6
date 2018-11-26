8288440fbf5dcf3eb8bc9f057604e15c85ada5c7
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-26 16:24:21 +0100

    Napravljena klasa Status

---
+++ src/ba/unsa/etf/rpr/tutorijal06/Status.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr.tutorijal06;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Status {
+    private SimpleStringProperty status=new SimpleStringProperty();
+
+    public String getStatus() {
+        return status.get();
+    }
+
+    public SimpleStringProperty statusProperty() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status.set(status);
+    }
+
+    public Status(String status) {
+        this.status =new SimpleStringProperty(status);
+    }
+    @Override
+    public String toString(){return status.get();}
+}
